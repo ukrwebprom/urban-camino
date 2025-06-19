289527c1d736257f493e099a20aa584ae275d608
Yurii Pochtiennykh <ukrwebprom@gmail.com>
2025-06-19 18:49:12 +0300

    initial
    initial

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import MapView from './MapView';
+
+function App() {
+  return (
+    <div style={{ height: "100vh", width: "100vw" }}>
+      <MapView />
+    </div>
+  );
+}
+
+export default App
---
+++ src/MapView.jsx
@@ -0,0 +1,25 @@
+import { MapContainer, TileLayer, Marker, Popup, GeoJSON } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import { routeGeoJson } from './route';
+
+function MapView() {
+  const position = [46.47682810156394, 30.7649023743933]; 
+
+  return (
+    <MapContainer center={position} zoom={13} style={{ height: "100vh", width: "100%" }}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; OpenStreetMap contributors'
+      />
+      <Marker position={position}>
+        <Popup>
+          Начало пути.
+        </Popup>
+      </Marker>
+
+      <GeoJSON data={routeGeoJson} />
+    </MapContainer>
+  );
+}
+
+export default MapView;
