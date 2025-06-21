7c423175cee42f0022ad9b5163a0205970ff80f3
Yurii Pochtiennykh <ukrwebprom@gmail.com>
2025-06-21 20:13:49 +0300

    install btn
    install btn

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,39 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import MapView from './components/MapView';
 import ControlPanel from './components/ControlPanel';
 
 function App() {
+  const [deferredPrompt, setDeferredPrompt] = useState(null);
+  const [installAvailable, setInstallAvailable] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('beforeinstallprompt', (e) => {
+      e.preventDefault();
+      setDeferredPrompt(e); // Сохраняем событие
+      setInstallAvailable(true); // Показать кнопку
+    });
+  }, []);
+
   return (
     <div style={{ height: "100vh", width: "100vw" }}>
       <MapView />
-      <ControlPanel />
+      <ControlPanel 
+      showInstall={installAvailable}
+      onInstallClick={() => {
+          if (deferredPrompt) {
+            deferredPrompt.prompt();
+            deferredPrompt.userChoice.then((choiceResult) => {
+              if (choiceResult.outcome === 'accepted') {
+                console.log('PWA установлена');
+              } else {
+                console.log('Пользователь отменил установку');
+              }
+              setInstallAvailable(false);
+              setDeferredPrompt(null);
+            });
+          }
+        }}
+      />
     </div>
   );
 }
--- src/components/ControlPanel.jsx
+++ src/components/ControlPanel.jsx
@@ -1,10 +1,15 @@
 import styles from './ControlPanel.module.css';
 
-function ControlPanel() {
+function ControlPanel({ showInstall, onInstallClick }) {
     return (
       <div className={styles.panel}>
         <button className={styles.button}>Начать путь</button>
         <button className={styles.button}>Завершить</button>
+        {showInstall && (
+        <button onClick={onInstallClick}>
+          Установить приложение
+        </button>
+      )}
       </div>
     );
   }
