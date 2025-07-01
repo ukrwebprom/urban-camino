a42ae3cb17d903c880819f5a3e6fba9d14d4dc9e
Yurii Pochtiennykh <ukrwebprom@gmail.com>
2025-07-01 10:44:23 +0300

    correct 100vh
    correct 100vh

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,18 @@ function App() {
     setStep('journey');
   }
   }, [selectedRouteId]);
+
+  useEffect(() => {
+    const setViewportHeight = () => {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    };
+  
+    setViewportHeight();
+    window.addEventListener('resize', setViewportHeight);
+  
+    return () => window.removeEventListener('resize', setViewportHeight);
+  }, []);
   
   return (
     <div className={styles.main}>
