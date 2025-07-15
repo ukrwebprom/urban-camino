e61b376ae9ebb0302a0822e7a157381569e169a1
Yurii Pochtiennykh <ukrwebprom@gmail.com>
2025-07-15 18:23:21 +0300

    login overlay update
    login overlay update

--- src/components/UserOverlay.jsx
+++ src/components/UserOverlay.jsx
@@ -1,11 +1,14 @@
 import styles from './UserOverlay.module.css';
 import closeIcon from '../assets/close.svg';
 import { useState } from 'react';
+import userIcon from '../assets/unknownuser.svg';
 
 function UserOverlay ({onClose}) {
     const [isLogin, setIsLogin] = useState(true);
     return (
         <div className={styles.main}>
+            <img src={userIcon} />
+            <p className={styles.loginDescription}>Your path matters. Log in to save your steps and earn your rewards.</p>
             <ul className={styles.modeSwitcher}>
                 <li className = {isLogin ? styles.activeItem: ''} onClick={() => setIsLogin(true)}>Log In</li>
                 <li className = {!isLogin ? styles.activeItem: ''} onClick={() => setIsLogin(false)}>Create account</li>
